8babc9c4f658ea26eeb96023a4742334ef37d133
SHORIF SHUVO <78609793+Shuvo441@users.noreply.github.com>
2022-03-03 17:57:40 +0600

    Add files via upload

---
+++ firebaseConfiguration.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDAmlwITqhjV3bKps6uDOhNClTTWnXS5aY",
+    authDomain: "chat-apps-95689.firebaseapp.com",
+    databaseURL: "https://chat-apps-95689-default-rtdb.firebaseio.com",
+    projectId: "chat-apps-95689",
+    storageBucket: "chat-apps-95689.appspot.com",
+    messagingSenderId: "906051527525",
+    appId: "1:906051527525:web:617ff6b547b9ef6b44f78c"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
